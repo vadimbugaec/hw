e11793c7cb750d72e7597d5b0f11a83854418b22
vadimbugaec <vadimbugaec@gmail.com>
2023-06-26 22:57:00 +0300

    add feat to output variable in diferent types

---
+++ hw3.2/hw3.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	double number = 3.14159;
+
+	std::cout << "doubleNumber: " << number << std::endl;
+
+	int convertedNumber = int(number);
+
+	std::cout << "intNumber: " << convertedNumber << std::endl;
+
+	std::cout << "$Number: "
+		<< std::setw(20) << std::right << std::setfill('$') << number << std::endl;
+	return 0;
+}
