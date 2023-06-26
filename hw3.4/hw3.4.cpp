2ef93038db1824f3913d522eccf06e83a5f73368
vadimbugaec <vadimbugaec@gmail.com>
2023-06-26 23:05:54 +0300

    Created enam with number values and int changed to bool. 1-7 true, other numbers is false

---
+++ hw3.4/hw3.4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+enum Weekdays
+{
+	Monday = 1,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday,
+	Sunday
+};
+
+int main()
+{
+	std::cout << "Numerical values of the days of the week:" << std::endl;
+	
+	std::cout << "Monday: " << int (Monday) << std::endl;
+	std::cout << "Tuesday: " << int (Tuesday) << std::endl;
+	std::cout << "Wednesday: " << int (Wednesday) << std::endl;
+	std::cout << "Thursday: " << int (Thursday) << std::endl;
+	std::cout << "Friday: " << int (Friday) << std::endl;
+	std::cout << "Saturday: " << int (Saturday) << std::endl;
+	std::cout << "Sunday: " << int (Sunday) << std::endl;
+
+	int number;
+	std::cout << "Enter number: ";
+	std::cin >> number;
+
+	bool trueorfalsevalue = number >= 1 && number <= 7;
+	std::cout << "Bool format value: " << std::boolalpha << trueorfalsevalue << std::endl;
+	return 0;
+}
