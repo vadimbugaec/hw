fdc62b24d787595965dd95006ff4f4bdb56c49e0
vadimbugaec <vadimbugaec@gmail.com>
2023-06-27 16:01:15 +0300

    removed aditional TAB

--- hw3.2/hw3.2.cpp
+++ hw3.2/hw3.2.cpp
@@ -3,15 +3,15 @@
 
 int main()
 {
-	double number = 3.14159;
+  double number = 3.14159;
 
-	std::cout << "doubleNumber: " << number << std::endl;
+  std::cout << "doubleNumber: " << number << std::endl;
 
-	int convertedNumber = int(number);
+  int convertedNumber = int(number);
 
-	std::cout << "intNumber: " << convertedNumber << std::endl;
+  std::cout << "intNumber: " << convertedNumber << std::endl;
 
-	std::cout << "$Number: "
+  std::cout << "$Number: "
 		<< std::setw(20) << std::right << std::setfill('$') << number << std::endl;
-	return 0;
+  return 0;
 }
