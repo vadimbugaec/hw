42a3d3216439e38c23089ac584f1ae3d74d1a453
vadimbugaec <vadimbugaec@gmail.com>
2023-06-28 23:39:26 +0300

    Create convertator from seconds to hours, min, sec

---
+++ hw4/hw4.1/hw4.1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    int Totalsec = 0;
+    std::cout << "Enter total seconds count:";
+    std::cin >> Totalsec;
+
+    int hours = Totalsec / 3600;
+    std::cout << hours << " hour ";
+
+    int min = Totalsec % 3600 / 60;
+    std::cout << min << " minutes ";
+
+    int sec = Totalsec % 3600 % 60;
+    std::cout << sec << " seconds";
+    
+    return 0;
+}
