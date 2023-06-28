348d24777a95ba542550bab2131674979e60c392
vadimbugaec <vadimbugaec@gmail.com>
2023-06-28 23:42:27 +0300

    Created simple comparing numbers

---
+++ hw4/hw4.3/hw4.3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+	int num = 0;
+	std::cout << "Enter first number: ";
+	std::cin >> num;
+
+	int num2 = 0;
+	std::cout << "Enter second number: ";
+	std::cin >> num2;
+
+	bool lessThan = num < num2;
+	bool equal = num == num2;
+	bool moreThan = num > num2;
+	bool lessOrEqual = num <= num2;
+
+	std::cout << "Num < num2: " << std::boolalpha << lessThan << std::endl;
+	std::cout << "Num = num2: " << std::boolalpha << equal << std::endl;
+	std::cout << "Num > num2: " << std::boolalpha << moreThan << std::endl;
+	std::cout << "Num <= num2: " << std::boolalpha << lessOrEqual << std::endl;
+
+	return 0;
+}
