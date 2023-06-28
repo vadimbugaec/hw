af65c26b7e3b496879a617b7924d843c5d8f8081
vadimbugaec <vadimbugaec@gmail.com>
2023-06-28 23:44:46 +0300

    Create calculator for square and length of circle

---
+++ hw4/hw4.5/hw4.5.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	int radius = 0;
+	std::cout << "Enter the radius of circle: ";
+	std::cin >> radius;
+
+	double p = 3.14;
+
+	double square = p * radius * radius;
+	double length = 2 * p * radius;
+
+	std::cout << "Square of the circle: " << square << std::endl;
+	std::cout << "Perimeter of the circle: " << length << std::endl;
+
+	return 0;
+}
