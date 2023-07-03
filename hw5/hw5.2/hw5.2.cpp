0092519b4c0e86cbb4e915fdae437ee7148e2367
vadimbugaec <vadimbugaec@gmail.com>
2023-07-04 02:22:46 +0300

    Read 3 numbers. Show min with ternary operator

---
+++ hw5/hw5.2/hw5.2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a, b;
+
+    std::cout << "Enter first number: ";
+    std::cin >> a;
+    std::cout << "Second number: ";
+    std::cin >> b;
+
+    int min = (a < b) ? a : b;
+    std::cout << "Min number: " << min << std::endl;
+
+    return 0;
+}
