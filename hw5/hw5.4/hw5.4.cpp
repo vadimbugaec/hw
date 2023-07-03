328c2960749318b4828f047f91aadc6fdbffc745
vadimbugaec <vadimbugaec@gmail.com>
2023-07-04 02:30:09 +0300

    Read 3 angles, check if they form a triangle

---
+++ hw5/hw5.4/hw5.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a, b, c;
+    std::cout << "Enter first angle:";
+    std::cin >> a;
+    std::cout << "Enter second angle:";
+    std::cin >> b;
+    std::cout << "Enter third angle:";
+    std::cin >> c;
+
+    if (a + b + c == 180)
+    {
+        std::cout << "Its a triangle" << std::endl;
+    }
+    else
+    {
+        std::cout << "Its not a triangle" << std::endl;
+    }
+    return 0;
+}
