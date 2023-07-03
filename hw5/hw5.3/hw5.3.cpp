159f9a66a3daf382077999c515517f99e45f7c38
vadimbugaec <vadimbugaec@gmail.com>
2023-07-04 02:25:44 +0300

    Сheck if a number is a multiple of 5 and 11

---
+++ hw5/hw5.3/hw5.3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a;
+    std::cout << "Enter number: ";
+    std::cin >> a;
+
+    if (a % 11 == 0 && a % 5 == 0)
+    {
+        std::cout << "The number is divisible by 5 and 11 at the same time" << std::endl;
+    }
+    else
+    {
+        std::cout << "The number isn`t divisible by 5 and 11 at the same time" << std::endl;
+    }
+    return 0;
+}
