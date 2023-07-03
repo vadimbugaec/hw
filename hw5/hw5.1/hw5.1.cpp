ed3b96081cd8d7d470dcfd30529fa00c80b6dc1e
vadimbugaec <vadimbugaec@gmail.com>
2023-07-04 02:20:55 +0300

    Read 3 numbers. Show max number

---
+++ hw5/hw5.1/hw5.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a, b, c;
+	
+	std::cout << "Enter first number: ";
+	std::cin >> a;
+	std::cout << "Enter second number: ";
+	std::cin >> b;
+	std::cout << "Enter third number: ";
+	std::cin >> c;
+
+	int max = a;
+
+	if (b > max)
+	{
+		max = b;
+	}
+	
+	if (c > max)
+	{
+		max = c;
+	}
+
+	std::cout << "Max number: " << max;
+	return 0;
+}
