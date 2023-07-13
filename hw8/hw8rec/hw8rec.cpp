9fe44c05b9031c0559f1dd1353de2ed38174ff07
vadimbugaec <vadimbugaec@gmail.com>
2023-07-13 22:28:26 +0300

    Recurursion tasks

---
+++ hw8/hw8rec/hw8rec.cpp
@@ -0,0 +1,23 @@
+﻿#include "hw8rec.h"
+
+int main()
+{
+	int num;
+	std::cout << "Enter a number: ";
+	std::cin >> num;
+	int result = factorialrec(num);
+	std::cout << num << "! = " << result << std::endl;
+
+
+	int N3;
+	std::cout << "Enter the value of N: ";
+	std::cin >> N3;
+	printNaturalNumbers3(N3);
+
+	int N4;
+	std::cout << std::endl << "Enter the value of N: ";
+	std::cin >> N4;
+	printNaturalNumbers4(N4);
+
+	return 0;
+}
