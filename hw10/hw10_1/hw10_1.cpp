91110660fb2916f32d6a2f0ce7e472250ff560b8
vadimbugaec <vadimbugaec@gmail.com>
2023-07-20 21:52:14 +0300

    funct void for swap numbers

---
+++ hw10/hw10_1/hw10_1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void swap2(int& a, int& b) 
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+
+    cout << "Enter first number: "; 
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Num1 = " << num1 << " and Num2 = " << num2 << endl;
+    swap(&num1, &num2);
+    cout << "After swap: " << "Num1 = " << num1 << " and Num2 = " << num2 << endl;
+    cout << "\nTHE END OF THE FIRST PART\n" << endl;
+
+    int x = 0;
+    int y = 0;
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+
+    std::cout << "X = " << x << " and Y = " << y << endl;
+    swap2(x, y);
+    std::cout << "After swap: X = " << x << " and Y = " << y << endl;
+}
