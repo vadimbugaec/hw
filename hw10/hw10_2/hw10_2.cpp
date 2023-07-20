b689e7e74ce4689245924e8356bf975037eade2c
vadimbugaec <vadimbugaec@gmail.com>
2023-07-20 21:53:24 +0300

    funct bool for sum of elements

---
+++ hw10/hw10_2/hw10_2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool calculateSum(double* arr, int arrSize, double& sum) 
+{
+    if (arrSize <= 0) 
+    {
+        return false;
+    }
+    sum = 0.0;
+    for (int i = 0; i < arrSize; i++) 
+    {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    return true;
+}
+
+int main() {
+    const int MAX = 5;
+    double arr[MAX];
+    int arrSize = 5;
+    double sum = 0.0;
+
+    if (calculateSum(arr, arrSize, sum)) 
+    {
+        cout << "Sum of elements: " << sum << endl;
+    }
+    else
+    {
+        cout << "Array size is not positive." << endl;
+    }
+
+    return 0;
+}
