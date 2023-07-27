5e544e52a84dcd0f5649e252b425eeb6f0e1ba0c
vadimbugaec <vadimbugaec@gmail.com>
2023-07-27 21:44:23 +0300

    bublesort for default array

---
+++ hw12/hw12_1/hw12_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void bubleSort(int arr[], int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+			int temp = arr[j];
+			arr[j] = arr[j + 1];
+			arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	const int arrsize = 10;
+	int bublearr[arrsize] = { 2, 5, 3, 1, 6, 8, 4, 7, 9, 10 };
+	bubleSort(bublearr, arrsize);
+	for (int i = 0; i < arrsize; i++)
+	{
+		cout << bublearr[i] << " ";
+	}
+	return 0;
+}
