a5a88e3ccb8f67590cf2d8e5314632037de78f78
vadimbugaec <vadimbugaec@gmail.com>
2023-08-06 19:52:25 +0300

    Revorked WORDLE

--- hw13/hw13.cpp
+++ hw13/hw13.cpp
@@ -4,10 +4,10 @@
 #include <ctime>
 #include <cstdlib>
 #include <fstream>
+#define NUMBER_OF_WORDS 10
 
 using namespace std;
 
-const char* database[] = { "WORD1", "WORD2", "WORDN" };
 
 int getDay(const tm* tmObj)
 {
@@ -25,30 +25,25 @@ int getYear(const tm* tmObj)
 }
 
 
-const char* getRandomWord() {
-    int index = rand() % (sizeof(database) / sizeof(database[0]));
-    return database[index];
-}
-
-string compareWords(const string& guessedWord, const string& secretWord) 
+string compareWords(const string& guessedWord, const string& secretWord)
 {
     string result;
-    if (guessedWord.length() != secretWord.length()) 
+    if (guessedWord.length() != secretWord.length())
     {
         return "Invalid word length";
     }
 
-    for (size_t i = 0; i < guessedWord.length(); i++) 
+    for (size_t i = 0; i < guessedWord.length(); i++)
     {
-        if (guessedWord[i] == secretWord[i]) 
+        if (guessedWord[i] == secretWord[i])
         {
             result += toupper(guessedWord[i]);
         }
-        else if (secretWord.find(guessedWord[i]) != string::npos) 
+        else if (secretWord.find(guessedWord[i]) != string::npos)
         {
             result += tolower(guessedWord[i]);
         }
-        else 
+        else
         {
             result += '*';
         }
@@ -57,64 +52,96 @@ string compareWords(const string& guessedWord, const string& secretWord)
     return result;
 }
 
-void saveWordOfDay(const string& word) 
+void saveWordOfDay(const string& word)
 {
-    ofstream outputFile("word_of_day.txt");
+    ofstream outputFile("word_of_day.txt", ios::app);
     if (outputFile.is_open()) {
-        outputFile << word;
+        outputFile << word << endl;
         outputFile.close();
     }
 }
 
 string readWordOfDay() {
+    srand((time(NULL)));
     ifstream inputFile("word_of_day.txt");
-    std::string word;
-    if (inputFile.is_open()) 
+    string words[NUMBER_OF_WORDS];
+    int numberOfRow = rand() % NUMBER_OF_WORDS, i = 0;
+    if (inputFile.is_open())
     {
-        inputFile >> word;
-        inputFile.close();
+        while (!inputFile.eof())
+        {
+            inputFile >> words[i];
+            i++;
+        }
     }
-    return word;
+    return words[numberOfRow];
+}
+string readWordOfDay(int number) {
+    ifstream inputFile("word_of_day.txt");
+    string words[10];
+    int i = 0;
+    if (inputFile.is_open())
+    {
+        while (!inputFile.eof())
+        {
+            inputFile >> words[i];
+            i++;
+        }
+    }
+    return words[number];
 }
 
-int main() 
+bool checkGuessedWordOfDay(tm *now)
 {
-    time_t t = time(nullptr);
-    tm* now = localtime(&t);
+    ifstream inputFile("guessed_or_not_word_of_day.txt");
+    string date = to_string(getDay(now)) + to_string(getMonth(now)) + to_string(getYear(now));
+    string dateFromFile;
+    if (inputFile.is_open())
+    {
+        while (!inputFile.eof())
+        {
+            inputFile >> dateFromFile;
+        }
+    }
+    if (date == dateFromFile)
+    {
+        return true;
+    }
+    return false;
+}
 
-    cout << "Current Date: " << getDay(now) << "-"
-        << getMonth(now) << "-" << getYear(now) << endl;
+void writeGuessed(tm* now)
+{
+    ofstream outputFile("guessed_or_not_word_of_day.txt");
+    string date = to_string(getDay(now)) + to_string(getMonth(now)) + to_string(getYear(now));
+    if (outputFile.is_open()) {
+        outputFile << date << endl;
+        outputFile.close();
+    }
+}
 
-    srand(static_cast<unsigned int>(time(0)));
-
+void Game(string wordOfDay, tm *now)
+{
     const int MAX_ATTEMPTS = 10;
     int attempts = 0;
-
-    string wordOfDay = readWordOfDay();
-    if (wordOfDay.empty()) 
-    {
-        wordOfDay = getRandomWord();
-        saveWordOfDay(wordOfDay);
-    }
-
-    cout << "WORDLE OF THE DAY "<< endl;
+    cout << "WORDLE OF THE DAY " << endl;
     cout << "TRY TO GUESS IT WITH " << MAX_ATTEMPTS << " ATEMPS." << endl;
 
     string guessedWord;
 
-    while (attempts < MAX_ATTEMPTS) 
+    while (attempts < MAX_ATTEMPTS)
     {
         cout << "Write word with " << wordOfDay.length() << " letters: ";
         cin >> guessedWord;
 
-        if (guessedWord == wordOfDay) 
+        if (guessedWord == wordOfDay)
         {
             cout << "Gratz, you guess the word! \"" << wordOfDay << "\"!\n";
             cout << "Atempt count: " << attempts + 1 << std::endl;
-            saveWordOfDay(getRandomWord());
+            writeGuessed(now);
             break;
         }
-        else 
+        else
         {
             string comparisonResult = compareWords(guessedWord, wordOfDay);
             cout << "Try № " << attempts + 1 << ": " << comparisonResult << endl;
@@ -123,10 +150,83 @@ int main()
         attempts++;
     }
 
-    if (attempts == MAX_ATTEMPTS) 
+    if (attempts == MAX_ATTEMPTS)
     {
         std::cout << "You didnt guess, the word was: \"" << wordOfDay << "\"\n";
     }
+}
+
+void wordOfDay(tm *now)
+{
+    string wordOfDay;
+    if (getDay(now) < NUMBER_OF_WORDS)
+    {
+        wordOfDay = readWordOfDay(getDay(now));
+    }
+    else
+    {
+        wordOfDay = readWordOfDay(getDay(now)% NUMBER_OF_WORDS);
+    }
+    if (checkGuessedWordOfDay(now))
+    {
+        cout << "You have already guessed the WORD OF THE DAY, come back tomorrow\n";
+        return;
+    }
+    Game(wordOfDay,now);
+    
+}
+
+void randomWord(tm *now)
+{
+    string wordOfDay = readWordOfDay();
+    Game(wordOfDay,now);
+}
+
+void menu(tm*& now)
+{
+    while (true)
+    {
+        cout << "Current Date: " << getDay(now) << "-"
+            << getMonth(now) << "-" << getYear(now) << endl;
+        char choice = 0;
+        cout << "1 - Word of the day" << "\n2 - Random Word" << "\n0 - Exit" << endl;
+        cin >> choice;
+        while (choice != 0)
+        {
+            switch (choice)
+            {
+            case '0':
+            {
+                return;
+            }
+            case '1':
+            {
+                wordOfDay(now);
+                break;
+            }
+            case '2':
+            {
+                randomWord(now);
+                break;
+            }
+            default:
+            {
+                cout << "Wrong number, try again" << endl;
+                break;
+            }
+            }
+            break;
+        }
+    }
+}
+
+    
+
+int main()
+{
+    time_t t = time(nullptr);
+    tm* now = localtime(&t);
+    menu(now);
 
     return 0;
 }
