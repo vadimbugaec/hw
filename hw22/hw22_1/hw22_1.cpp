2ffdf17682957c385abee72cfcf2d589661aaaa9
vadimbugaec <vadimbugaec@gmail.com>
2023-08-31 20:28:37 +0300

    Counter of unique words

---
+++ hw22/hw22_1/hw22_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <sstream>
+
+using namespace std;
+
+unsigned int uniqueWordsCount(const string& line) {
+    istringstream iss(line);
+    string word;
+    set<string> uniqueWords;
+
+    while (iss >> word) 
+    {
+        uniqueWords.insert(word);
+    }
+
+    return uniqueWords.size();
+}
+
+int main() {
+    string line = "Hello Hey Hello Bye Hey Hi Hey Hey Hi Hey Hi Go Go Go";
+    unsigned int count = uniqueWordsCount(line);
+    cout << "Count of unique words: " << count << endl;
+    return 0;
+}
