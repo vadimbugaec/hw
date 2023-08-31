360f83e4635c31966679cf969c2e753830b1cac4
vadimbugaec <vadimbugaec@gmail.com>
2023-08-31 20:28:59 +0300

    Check most used word

---
+++ hw22/hw22_2/hw22_2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+#include <cstring>
+#include <cctype>
+#include <algorithm>
+#include <unordered_map>
+#include <sstream>
+
+using namespace std;
+
+string mostOccuredWord(const string& line) 
+{
+    unordered_map<string, int> wordCount;
+    istringstream iss(line);
+    string word;
+
+    while (iss >> word) 
+    {
+        string cleanedWord;
+        for (char c : word) {
+            if (isalpha(c) || isdigit(c)) 
+            {
+                cleanedWord += c;
+            }
+        }
+        if (!cleanedWord.empty()) {
+            wordCount[cleanedWord]++;
+        }
+    }
+
+    string mostCommonWord;
+    int maxCount = 0;
+    for (const auto& pair : wordCount) 
+    {
+        if (pair.second > maxCount) 
+        {
+            maxCount = pair.second;
+            mostCommonWord = pair.first;
+        }
+    }
+    return mostCommonWord;
+}
+
+int main() 
+{
+    
+    string line = "Hi, Hi, hi, I dont know what sentence to write so i`ll just leave this nonsense here)";
+    transform(line .begin(), line.end(), line.begin(), ::tolower);
+    string mostCommon = mostOccuredWord(line);
+    cout << "Most used word: " << mostCommon << endl;
+
+    return 0;
+}
